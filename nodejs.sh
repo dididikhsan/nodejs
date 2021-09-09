cd1ee5a519ec28d6f09dfc034b54e2391e4b2967
dididikhsan <76654828+dididikhsan@users.noreply.github.com>
2021-09-09 17:44:11 +0700

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -10,14 +10,14 @@ VERSION=v6.11.3;
 
 # Creates directory for downloads, and downloads node
 cd ~/ && mkdir temp && cd temp;
-wget https://nodejs.org/dist/latest-v10.x/node-v10.24.1-linux-armv6l.tar.gz;
-tar -xzf node-v10.24.1-linux-armv6l.tar.gz;
+wget https://nodejs.org/dist/latest-v7.x/node-v7.10.1-linux-armv6l.tar.gz;
+tar -xzf node-v7.10.1-linux-armv6l.tar.gz;
 # Remove the tar after extracing it.
-sudo rm node-v10.24.1-linux-armv6l.tar.gz;
+sudo rm node-v7.10.1-linux-armv6l.tar.gz;
 # This line will clear existing nodejs
 sudo rm -rf /opt/nodejs;
 # This next line will copy Node over to the appropriate folder.
-sudo mv node-v10.24.1-linux-armv6l /opt/nodejs/;
+sudo mv node-v7.10.1-linux-armv6l /opt/nodejs/;
 # Remove existing symlinks
 sudo unlink /usr/bin/node;
 sudo unlink /usr/sbin/node;
